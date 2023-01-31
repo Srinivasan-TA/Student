6580b839a0adbbd9f9d4394f69f32b315a88970e
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-01-31 12:09:38 +0530

    2nd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 class Student extends Main {
@@ -24,10 +25,18 @@ class Student extends Main {
 
 class Main{
     public static void main(String[] args) {
-    Logger l = Logger.getLogger("com.api.jar");
-        Student student = new Student("John Doe", 12, 3.5);
-        l.info(student.getInfo());
-        student.updateGPA(3.7);
-        l.info(student.getInfo());
+        Scanner se = new Scanner(System.in);
+        Logger l = Logger.getLogger("com.api.jar");
+        l.info("enter your name :");
+            String name = se.next();
+            l.info("enter your grade level :");
+            int gradelevel = se.nextInt();
+            l.info("Type the gpa :");
+            double gpa= se.nextDouble();
+            Student student = new Student(name,gradelevel,gpa);
+            l.info(student.getInfo());
+            student.updateGPA(3.7);
+            l.info(student.getInfo());
+        }
+
     }
-}
